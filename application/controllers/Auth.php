ad5de03122dd381bcc899a3621d230ead496624f
guntekhunter <72636466+guntekhunter@users.noreply.github.com>
2021-03-06 22:30:24 +0800

    beerhasil membuat halaman register

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,6 +10,8 @@ class Auth extends CI_Controller
     }
     public function index()
     {
+        //untuk membuat judul pada title halaman
+        //$data dikirim ke setiap header agar halaman sesuai dengan title masing masing
         $data['title'] = 'Login user';
         $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
@@ -17,8 +19,18 @@ class Auth extends CI_Controller
     }
     public function register()
     {
+        //validasi data nama harus diisi didalam form
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim', 'valid_email');
+        //untuk mengecek validasi data email unik atau tidak, dan emailnya sesuai dengan format email
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'email ini sudah pernah digunakan'
+        ]);
+        //validasi untuk minimal panjang karakter dan kesamaan dengan password satunya 
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'password tidak sama!',
+            'min_length' => 'password terlalu pendek!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 
         if ($this->form_validation->run() == false) {
             $data['title'] = 'Registrasi user';
@@ -26,7 +38,19 @@ class Auth extends CI_Controller
             $this->load->view('auth/register');
             $this->load->view('templates/auth_footer');
         } else {
-            echo 'alhamdulillah';
+            $data =
+                [
+                    'name' => $this->input->post('name'),
+                    'email' => $this->input->post('email'),
+                    'image' => 'default.jpg',
+                    'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+
+                ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            akun anda berhasil dibuat</div>');
+            redirect('auth');
         }
     }
 }
--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -12,20 +12,23 @@
                         <form class="user" method="post" action="<?= base_url('auth/register'); ?>">
                             <div class="form-group row">
                                 <div class="col-sm-12 mb-3 mb-sm-0">
-                                    <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="masukan nama..">
-                                    <?= form_error('name'); ?>
+                                    <input type="text" class="form-control form-control-user" value="<?= set_value('name') ?>" id="name" name="name" placeholder="masukan nama..">
+                                    <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                                 </div>
 
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control form-control-user" id="email" name='email' placeholder="masukkan email..">
+                                <input type="text" class="form-control form-control-user" value="<?= set_value('email') ?>" id="email" name='email' placeholder="masukkan email..">
+                                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
                                     <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
+                                    <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
                                 </div>
                                 <div class="col-sm-6">
                                     <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Ulangi Password">
+
                                 </div>
                             </div>
                             <button type="submit" class="btn btn-primary btn-user btn-block">
